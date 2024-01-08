4d2c5a7e67c79556972c24106d3471b6179e4821
whandril-matos <whandrilmatos@gmail.com>
2024-01-08 20:35:31 -0300

    08012024

--- src/JS/hiddenCharacters.js
+++ src/JS/hiddenCharacters.js
@@ -1,128 +1,44 @@
 document.addEventListener("DOMContentLoaded", function (){
-  //   // define a class 
-  //   class Pessoa {
-  //      // Construtor to start the instance 
-  //     constructor() {
-  //         this.bT = document.querySelectorAll(".navLinkFade")
-  //         this.bD = document.querySelectorAll(".fullBody")
-  //         this.text = document.querySelector(".rowText")
-  //         this.imgC = document.querySelector(".rowImg")
-  //       }
-    
-  //       adicionarOuvintes() {
-  //         this.bT.forEach((imagem, indice) => {
-  //           imagem.addEventListener('click', () => {
-  //             this.bodyInIdex(indice);
-  //             this.ImgCop(indice);
-     
+// passo 1 - dar um jeito de pegar o elemento HTML dos botões
+  const bt = document.querySelectorAll('.navLinkFade');
+  const inSquare = document.querySelectorAll('.inBL');
+  const fullBody = document.querySelectorAll('.fullBody');
 
-  //           });
-  //         });
-  //       }
-        
-  //       ImgCop(indice){
-  //         this.bD[indice].classList.add("onIndex")
-  //         this.text.classList.add("onIndex")
-  //         this.imgC.classList.add("onIndex")
-          
-  //       }
+  bt.forEach((inBt, indice) => {
+      inBt.addEventListener('click', () => {
+        hiddenHero()
+        hiddenBackGround()
 
-  //       bodyInIdex(indice) {
-  //         this.onIndex = document.querySelectorAll('.onIndex');
-          
-  //         this.onIndex.forEach((elemento) => {
-  //           console.log(elemento)
-  //           elemento.classList.remove("onIndex");
-  //           console.log(elemento)
-            
-  //         });
- 
-  //       }
-        
+        showHero(indice)
+        showBackGround()
 
-  //   }
-  
-  // //Creates an instance of the class
-  // const pessoa1 = new Pessoa();
+      })
+  })
 
-  // // Call the method of the class 
-  // pessoa1.adicionarOuvintes(); 
-  
-  // const bT = document.querySelectorAll(".navLinkFade")
-  // const bD = document.querySelectorAll(".fullBody")
-  // const text = document.querySelector(".rowText")
-  // const imgC = document.querySelector(".rowImg")
- 
-  // function adicionarOuvintes() {
-  //   bT.forEach((imagem, indice) => {
-  //     imagem.addEventListener('click', () => {
-  //       bodyInIdex(indice);
-  //       ImgCop(indice);
-
-  //     });
-  //   });
-  // }
-  // function ImgCop(indice){
-  //   text.classList.add("onIndex")
-  //   imgC.classList.add("onIndex")
-  //   bD[indice].classList.add("onIndex")
-    
-      
-  // }
-  // function bodyInIdex() {
-  //   let onIndex = document.querySelectorAll('.onIndex');
-  //   onIndex.forEach((elemento) => {
-  //     elemento.classList.remove("onIndex");
-      
-  //   });
-    
-  //   console.log(onIndex)
-  // }
-  // function main(){
-  //   bodyInIdex()
-  //   adicionarOuvintes()
-  // }
-  // main()
-
-  const bT = document.querySelectorAll(".navLinkFade")
-  const bD = document.querySelectorAll(".fullBody")
-  const text = document.querySelector(".rowText")
-  const imgC = document.querySelector(".rowImg")
- 
-  function adicionarOuvintes() {
-      bT.forEach((imagem, indice) => {
-        imagem.addEventListener('click', () => {
-          bodyInIdex()
-         add(indice) 
-          
-        });
-      });
+  function showHero(indice){
+    fullBody[indice].classList.add('atc')
   }
 
-  function bodyInIdex() {
-      let onIndex = document.querySelectorAll('.onIndex');
-      onIndex.forEach((elemento) => {
-        elemento.classList.remove("onIndex");
-        text.classList.toggle("onIndex")
-        imgC.classList.toggle("onIndex")
-      });
-    }
-  function add(indice){
-    bD[indice].classList.add("onIndex")
-    
-
+  function showBackGround(){
+    inSquare.classList.add('onIndex');
   }
-  
-  function pInterval (){
-    let i = 0
-    while (i <= 2){
-      
-      i++
 
+  function hiddenHero(){
+    let hero = document.querySelectorAll('.atc');
+    if (hero){
+      hero.classList.remove('atc');
     }
     
   }
+  function hiddenBackGround(){
+    let backGround = document.querySelectorAll('.onIndex');
+    backGround.classList.remove('onIndex');
+    if (backGround){
+      backGround.classList.remove('atc');
+    }
+  }
 
-  adicionarOuvintes()
 
+
+  
 })
