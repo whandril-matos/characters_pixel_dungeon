3ab25fdd3761554edcb1d46fced1561fe4b97e3d
whandril-matos <whandrilmatos@gmail.com>
2024-01-13 21:55:41 -0300

    13012024

--- src/JS/hiddenCharacters.js
+++ src/JS/hiddenCharacters.js
@@ -1,5 +1,76 @@
-document.addEventListener("DOMContentLoaded", function (){
-// passo 1 - dar um jeito de pegar o elemento HTML dos botões
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   // passo 1 - dar um jeito de pegar o elemento HTML dos botões
+//   const bt = document.querySelectorAll('.navLinkFade');
+//   const inSquare = document.querySelectorAll('.inBL');
+//   const fullBody = document.querySelectorAll('.fullBody');
+
+//   bt.forEach((inBt, indice) => {
+//       inBt.addEventListener('click', () => {
+//         hiddenHero()
+//         hiddenBackGround()
+
+//         showHero(indice)
+//         showBackGround()
+
+//       })
+//   })
+
+//   function showHero(indice){
+//     fullBody[indice].classList.add('atc')
+//   }
+
+//   function showBackGround(){
+//     inSquare.classList.add('onIndex');
+//   }
+
+//   function hiddenHero(){
+//     // const hero = document.querySelectorAll('.atc');
+//     // hero.classList.remove('atc');
+
+//     let heroes = document.querySelectorAll('.atc');
+
+//     if (heroes.length > 0) {
+//       // Existe pelo menos um elemento com a classe '.atc'
+//       Array.from(heroes).forEach(hero => {
+//           hero.classList.remove('atc');
+//       });
+//     } else {
+//         // Não há elementos com a classe '.atc'
+//         console.log("Nenhum elemento com a classe '.atc' encontrado.");
+//     }
+    
+    
+//   }
+
+//   function hiddenBackGround(){
+//     // let backGround = document.querySelectorAll('.onIndex');
+//     // backGround.classList.remove('onIndex');
+//     // if (backGround){
+//     //   backGround.classList.remove('atc');
+//     // }
+//     let heroes = document.querySelectorAll('.onIndex');
+
+//     if (heroes.length > 0) {
+//       // Existe pelo menos um elemento com a classe '.atc'
+//       Array.from(heroes).forEach(hero => {
+//           hero.classList.remove('onIndex');
+//       });
+//     } else {
+//         // Não há elementos com a classe '.atc'
+//         console.log("Nenhum elemento com a classe '.onIndex' encontrado.");
+//     }
+
+//   }
+
+
+
+
+// });
+
+
+document.addEventListener("DOMContentLoaded", function() {
   const bt = document.querySelectorAll('.navLinkFade');
   const inSquare = document.querySelectorAll('.inBL');
   const fullBody = document.querySelectorAll('.fullBody');
@@ -10,35 +81,33 @@ document.addEventListener("DOMContentLoaded", function (){
         hiddenBackGround()
 
         showHero(indice)
-        showBackGround()
-
+        showBackGround(indice)
       })
   })
 
   function showHero(indice){
-    fullBody[indice].classList.add('atc')
+    fullBody[indice].classList.add('atc');
   }
 
-  function showBackGround(){
-    inSquare.classList.add('onIndex');
+  function showBackGround(indice){
+    inSquare[indice].classList.add('onIndex');
   }
 
   function hiddenHero(){
-    let hero = document.querySelectorAll('.atc');
-    if (hero){
-      hero.classList.remove('atc');
-    }
-    
+    let heroes = document.querySelectorAll('.atc');
+    if (heroes.length > 0) {
+      Array.from(heroes).forEach(hero => {
+          hero.classList.remove('atc');
+      });
+    } 
   }
+
   function hiddenBackGround(){
-    let backGround = document.querySelectorAll('.onIndex');
-    backGround.classList.remove('onIndex');
-    if (backGround){
-      backGround.classList.remove('atc');
-    }
+    let heroes = document.querySelectorAll('.onIndex');
+    if (heroes.length > 0) {
+      Array.from(heroes).forEach(hero => {
+          hero.classList.remove('onIndex');
+      });
+    } 
   }
-
-
-
-  
-})
+});
